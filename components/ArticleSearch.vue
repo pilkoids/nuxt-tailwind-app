ec4b230815c73b405449ca0ca15af78089860057
grenville-e <grenville.eustace@gmail.com>
2020-02-22 18:08:38 -0500

    Updates to axios requests

--- components/ArticleSearch.vue
+++ components/ArticleSearch.vue
@@ -15,6 +15,9 @@
                 <p>{{n.url}}</p>
                 <p>{{n.description}}</p>
                 <p>Polarity: {{n.polarity}}</p>
+                <p>test123: {{n.test123}}</p>
+                <p>artSize: {{n.artSize}}</p>
+                                <p>counter: {{n.counter}}</p>
             </li>
         </ul>
     </div>
@@ -28,7 +31,8 @@ export default {
     data(){
         return{
             name:[],
-            qry:""
+            qry:"",
+            polarity:"init"
         }
     },
     mounted(){
@@ -38,56 +42,66 @@ export default {
         search(){
             console.log("Search clicked");
             console.log("Value:", this.qry)
-            this.getArticles(this.qry);
+            this.getArticles();
         },
         getArticles(){
-        axios({
-            "method":"GET",
-            "url":"https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search",
-            "headers":{
-            "content-type":"application/octet-stream",
-            "x-rapidapi-host":"microsoft-azure-bing-news-search-v1.p.rapidapi.com",
-            "x-rapidapi-key":"QIiovCTHWrmshe86Hfn2WFwYBPrZp1MqwVTjsnBYXNuAy0zGFS"
-            },"params":{
-                "count":"1",
-                "q":this.qry
-            }
-            })
-            .then((response)=>{
-                this.name = response.data.value;
-                for(var article in this.name){
-                    console.log("article url:",this.name[article].url);
+            axios({
+                "method":"GET",
+                "url":"https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search",
+                "headers":{
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":"microsoft-azure-bing-news-search-v1.p.rapidapi.com",
+                "x-rapidapi-key":"QIiovCTHWrmshe86Hfn2WFwYBPrZp1MqwVTjsnBYXNuAy0zGFS"
+                },"params":{
+                    "count":"3",
+                    "q":this.qry
+                }
+                })
+                .then((response)=>{
+                    //this.name = response.data.value;
+                    var articles = response.data.value;
+                    var counter = 1;
+                    for(var article in articles){
+                        console.log("article url:",articles[article].url);
+                        //articles[article].polarity = "initial";
 
-                    axios({
-                        "method":"GET",
-                        "url":"https://aylien-text.p.rapidapi.com/sentiment",
-                        "headers":{
-                        "content-type":"application/octet-stream",
-                        "x-rapidapi-host":"aylien-text.p.rapidapi.com",
-                        "x-rapidapi-key":"QIiovCTHWrmshe86Hfn2WFwYBPrZp1MqwVTjsnBYXNuAy0zGFS"
-                        },"params":{
-                        "url":this.name[article].url,
-                        "mode":"tweet"
-                        }
+                        axios({
+                            "method":"GET",
+                            "url":"https://aylien-text.p.rapidapi.com/sentiment",
+                            "headers":{
+                            "content-type":"application/octet-stream",
+                            "x-rapidapi-host":"aylien-text.p.rapidapi.com",
+                            "x-rapidapi-key":"QIiovCTHWrmshe86Hfn2WFwYBPrZp1MqwVTjsnBYXNuAy0zGFS"
+                            },"params":{
+                            "url":articles[article].url,
+                            "mode":"tweet"
+                            }
                         })
                         .then((response)=>{
                             console.log("Aylien response:", response.data.polarity);
-                            this.name[article].polarity = response.data.polarity;
-
+                            articles[article].polarity = response.data.polarity;
+                        // this.polarity = response.data.polarity;
                             console.log("result---------------", response);
-                            console.log("polarity:::", this.name[article].polarity)
-                            console.log("this.name", this.name)
+                            console.log("polarity:::", articles[article].polarity)
+                            console.log("this.name", articles);
+                            articles[article].test123 = "asdfasdfads";
+                            articles[article].artSize = articles.length;
+                            articles[article].counter = counter;
+                            if(counter == articles.length){
+                                this.name = articles;
+                            }
+
+                            counter++;
                         })
                         .catch((error)=>{
                             console.log(error)
                         })
+                    }
 
-                    this.name[article].test123 = "asdfasdfads";
-                }
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
+                })
+                .catch((error)=>{
+                    console.log(error)
+                })
         }
     }
 }
